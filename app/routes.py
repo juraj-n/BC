89296bd8ffd6ce4758c8e930ce81108dab4e045c
JurajNznsk <juraj.niznansky@gmail.com>
2026-03-21 17:07:25 +0100

    Basic .csv parsing

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,21 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
+from .utils import parse_csv
 
 main = Blueprint("main", __name__)
 
 @main.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("home.html")
+
+@main.route("/spectra", methods=["POST"])
+def spectra():
+    file = request.files.get("file")
+    if not file:
+        return "No file uploaded", 400
+    
+    try:
+        data = parse_csv(file)
+    except ValueError as e:
+        return str(e), 400
+    
+    return render_template("spectra.html", data=data)
---
+++ app/utils.py
@@ -0,0 +1,20 @@
+import csv
+
+def parse_csv(file):
+    #TODO: Propper CSV parsing
+    if not file.filename.endswith(".csv"):
+        raise ValueError("Invalid file type")
+    
+    data = []
+    stream = file.stream.read().decode("utf-8").splitlines()
+    reader = csv.reader(stream)
+
+    for row in reader:
+        try:
+            x = float(row[0])
+            y = float(row[1])
+            data.append([x,y])
+        except:
+            continue
+
+    return data
