e2989019ca03051d3056b39c36f3faebcf524bfb
JurajNznsk <juraj.niznansky@gmail.com>
2026-03-25 16:04:07 +0100

    Files upload - file name shows under VZORKY

--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,21 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, redirect, url_for
 from .utils import parse_csv
 
 main = Blueprint("main", __name__)
 
+uploaded_files = []
+
 @main.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("index.html", files=uploaded_files)
 
-@main.route("/upload", methods=["POST"])
-def upload():
-    return render_template("index.html")
+@main.route("/spectra", methods=["POST"])
+def spectra():
+    files = request.files.getlist("files")
+
+    global uploaded_files
+    for file in files:
+        if file and file.filename != "":
+            uploaded_files.append(file)
+
+    return redirect(url_for("main.home"))
